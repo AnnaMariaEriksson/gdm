e112390217ce75d08bf8d6c0b4099ae39b8b4830
Anna-Maria Eriksson <greatdanemaniac@gmail.com>
2021-06-13 20:53:45 +0200

    First commit - again. Re-added the whole project once again.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>Hello and welcome.</h1>
+    <h2>Soon you'll find some awesomesauce stuff here.</h2>
+    <p>I'm very excited about this!</p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
-  },
+  name: "Home"
 };
 </script>
