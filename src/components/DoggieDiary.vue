3d68cce1c9c55ce7647243aad06a4cf1b2619b08
Anna-Maria Eriksson <greatdanemaniac@gmail.com>
2021-08-01 15:42:13 +0200

    Added a login page for the doggie diary

---
+++ src/components/DoggieDiary.vue
@@ -0,0 +1,33 @@
+<template>
+<div>
+  <p>Moo!</p>
+<b-form id="login">
+  <b-form-group id="username" label="Username">
+    <b-form-input placeholder="username" v-model="username"></b-form-input>
+  </b-form-group>
+  <b-form-group id="password" label="Password">
+    <b-form-input type="password" placeholder="password..." v-model="password"></b-form-input>
+  </b-form-group>
+  <b-button id="sign-in">Sign in</b-button>
+</b-form>
+</div>
+</template>
+
+<script>
+export default {
+  name: "DoggieDiary",
+  data() {
+    return {
+      username: "",
+      password: ""
+    }
+  }
+};
+</script>
+
+<style scoped>
+form {
+  width: 50%;
+  margin: auto;
+}
+</style>
